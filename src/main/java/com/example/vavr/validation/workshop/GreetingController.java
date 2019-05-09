d9a0ac66b4327b7ce8752ee5d17194dc39e2b959
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-05-09 19:52:50 +0200

    draft 3

--- src/main/java/com/example/vavr/validation/workshop/GreetingController.java
+++ src/main/java/com/example/vavr/validation/workshop/GreetingController.java
@@ -1,10 +1,15 @@
 package com.example.vavr.validation.workshop;
 
 import com.example.vavr.validation.workshop.person.PersonRequest;
+import com.example.vavr.validation.workshop.person.PersonRequestValidation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static io.vavr.API.*;
+import static io.vavr.Patterns.$Invalid;
+import static io.vavr.Patterns.$Valid;
 
 /**
  * Created by mtumilowicz on 2019-05-08.
@@ -13,7 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     @PostMapping("/")
-    public ResponseEntity<PersonRequest> greeting(@RequestBody PersonRequest personRequest) {
-        return ResponseEntity.ok(personRequest);
+    public ResponseEntity greeting(@RequestBody PersonRequest personRequest) {
+        return Match(PersonRequestValidation.validate(personRequest)).of(
+                Case($Valid($()), v -> ResponseEntity.ok(v)),
+                Case($Invalid($()), iv -> ResponseEntity.badRequest().body(iv))
+        );
     }
 }
