7ddeccfe314889351a47da3d8998b4e7ec758658
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-05-11 17:17:03 +0200

    AddressMapper.java - removing null check

--- src/main/java/com/example/vavr/validation/workshop/person/domain/AddressMapper.java
+++ src/main/java/com/example/vavr/validation/workshop/person/domain/AddressMapper.java
@@ -1,22 +1,15 @@
 package com.example.vavr.validation.workshop.person.domain;
 
-import io.vavr.control.Option;
-
-import static io.vavr.API.*;
-import static io.vavr.Predicates.isNotNull;
+import lombok.NonNull;
 
 /**
  * Created by mtumilowicz on 2019-05-11.
  */
 class AddressMapper {
-    static Option<Address> mapFrom(NewAddressCommand command) {
-        return Match(command).option(
-                Case($(isNotNull()), address ->
-                        Address.builder()
-                                .city(address.getCity())
-                                .postalCode(address.getPostalCode())
-                                .build()
-                )
-        );
+    static Address mapFrom(@NonNull NewAddressCommand command) {
+        return Address.builder()
+                .city(command.getCity())
+                .postalCode(command.getPostalCode())
+                .build();
     }
 }
