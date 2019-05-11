597355ee35fb9c61b32473ab3d3634836f9e6237
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-05-11 17:17:45 +0200

    person mapper in service

--- src/main/java/com/example/vavr/validation/workshop/person/domain/PersonService.java
+++ src/main/java/com/example/vavr/validation/workshop/person/domain/PersonService.java
@@ -6,10 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
-import static io.vavr.API.*;
-import static io.vavr.Predicates.isNotNull;
-import static io.vavr.Predicates.isNull;
-
 /**
  * Created by mtumilowicz on 2019-05-09.
  */
@@ -20,23 +16,6 @@ public class PersonService {
     PersonRepository personRepository;
 
     public PersonId save(NewPersonCommand newPersonCommand) {
-        return personRepository.save(mapToPerson(newPersonCommand)).getId();
-    }
-
-    private Person mapToPerson(NewPersonCommand newPersonCommand) {
-        return Person.builder()
-                .name(newPersonCommand.getName())
-                .age(newPersonCommand.getAge())
-                .address(Match(newPersonCommand.getAddress()).of(
-                        Case($(isNull()), () -> null),
-                        Case($(isNotNull()), address ->
-                                Address.builder()
-                                        .city(address.getCity())
-                                        .postalCode(address.getPostalCode())
-                                        .build()
-                        )
-                ))
-                .emails(newPersonCommand.getEmails())
-                .build();
+        return personRepository.save(PersonMapper.mapFrom(newPersonCommand)).getId();
     }
 }
