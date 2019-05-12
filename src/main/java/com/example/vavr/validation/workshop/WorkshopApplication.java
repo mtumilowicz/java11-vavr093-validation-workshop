acaa19159a6aa3ddbf8bca7b42d84c1f743ba254
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-05-12 13:06:44 +0200

    infrastructure: JacksonConfig.java

--- src/main/java/com/example/vavr/validation/workshop/WorkshopApplication.java
+++ src/main/java/com/example/vavr/validation/workshop/WorkshopApplication.java
@@ -1,10 +1,7 @@
 package com.example.vavr.validation.workshop;
 
-import com.fasterxml.jackson.databind.Module;
-import io.vavr.jackson.datatype.VavrModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WorkshopApplication {
@@ -13,8 +10,4 @@ public class WorkshopApplication {
         SpringApplication.run(WorkshopApplication.class, args);
     }
 
-    @Bean
-    Module vavrModule() {
-        return new VavrModule();
-    }
 }
