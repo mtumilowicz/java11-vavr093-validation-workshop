49708fd32ba15c463f09aaeb5986b9bc83f7e78a
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-05-13 23:51:33 +0200

    EmailTest.groovy

---
+++ src/test/groovy/com/example/vavr/validation/workshop/person/patterns/EmailTest.groovy
@@ -0,0 +1,77 @@
+package com.example.vavr.validation.workshop.person.patterns
+
+import com.example.vavr.validation.workshop.intrastructure.NewPersonRequestValidationException
+import io.vavr.collection.List
+import io.vavr.control.Validation
+import spock.lang.Specification
+
+/**
+ * Created by mtumilowicz on 2019-05-13.
+ */
+class EmailTest extends Specification {
+
+    def "validate - valid"() {
+        given:
+        def stringEmails = List.of(
+                'aaa@aaa.pl',
+                'bbb@bbb.pl'
+        )
+        def emails = List.of(
+                Email.of('aaa@aaa.pl'),
+                Email.of('bbb@bbb.pl')
+        )
+
+        expect:
+        Email.validate(stringEmails) == Validation.valid(new Emails(emails))
+    }
+
+    def "validate - invalid"() {
+        given:
+        def stringEmails = List.of(
+                'a',
+                'b'
+        )
+        def errors = List.of(
+                'Email: a is not valid!',
+                'Email: b is not valid!'
+        )
+
+        expect:
+        Email.validate(stringEmails) == Validation.invalid(errors)
+    }
+
+    def "validateWorkshop - valid"() {
+        given:
+        def stringEmails = List.of(
+                'aaa@aaa.pl',
+                'bbb@bbb.pl'
+        )
+        def emails = List.of(
+                Email.of('aaa@aaa.pl'),
+                Email.of('bbb@bbb.pl')
+        )
+
+        expect:
+        Email.validateWorkshop(stringEmails) == new Emails(emails)
+    }
+
+    def "validateWorkshop - invalid"() {
+        given:
+        def stringEmails = List.of(
+                'a',
+                'b'
+        )
+        def errors = List.of(
+                'Email: a is not valid!',
+                'Email: b is not valid!'
+        )
+
+        when:
+        Email.validateWorkshop(stringEmails)
+
+        then:
+        NewPersonRequestValidationException ex = thrown()
+        ex.getErrors() == errors
+    }
+
+}
