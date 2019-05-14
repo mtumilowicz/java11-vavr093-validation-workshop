13979515f9ea736392ba9ca0bafea96aeea11a5e
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-05-14 07:39:03 +0200

    NameTest.groovy

---
+++ src/test/groovy/com/example/vavr/validation/workshop/person/patterns/NameTest.groovy
@@ -0,0 +1,37 @@
+package com.example.vavr.validation.workshop.person.patterns
+
+import com.example.vavr.validation.workshop.intrastructure.NewPersonRequestValidationException
+import io.vavr.collection.List
+import io.vavr.control.Validation
+import spock.lang.Specification
+
+/**
+ * Created by mtumilowicz on 2019-05-13.
+ */
+class NameTest extends Specification {
+
+    def "validate - valid"() {
+        expect:
+        Name.validate('Alfred') == Validation.valid(Name.of('Alfred'))
+    }
+
+    def "validate - invalid"() {
+        expect:
+        Name.validate('%') == Validation.invalid('Name: % is not valid!')
+    }
+
+    def "validateWorkshop - valid"() {
+        expect:
+        Name.validateWorkshop('Alfred') == Name.of('Alfred')
+    }
+
+    def "validateWorkshop - invalid"() {
+        when:
+        Name.validateWorkshop('%')
+
+        then:
+        NewPersonRequestValidationException ex = thrown()
+        ex.getErrors() == List.of('Name: % is not valid!')
+    }
+    
+}
